6081b5f049fa4dd7fb9d5f082b2c679fcdfefda2
AbhiroopS <abhiroopshome@gmail.com>
2025-04-13 12:59:36 +0530

    Implemented Ping Slash command

--- app.py
+++ app.py
@@ -2,28 +2,34 @@ import os
 import logging
 
 import discord
+from discord import app_commands
+
 from dotenv import load_dotenv
 
 load_dotenv("./.env")
 
 TOKEN = os.getenv("TOKEN")
 
+intents = discord.Intents.default()
+intents.message_content = True
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
+        await tree.sync()
         logging.info(f"Logged on as {self.user}")
 
-    async def on_message(self, message):
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == "ping":
-            logging.info(f"{message.author} said ping")
-            await message.channel.send("pong")
-
-
-intents = discord.Intents.default()
-intents.message_content = True
 client = MyClient(intents=intents)
+tree = app_commands.CommandTree(client)
+
+@tree.command(
+    name="ping",
+    description="Ping the bot",
+)
+async def ping(interaction):
+    await interaction.response.send_message("pong!")
+    logging.info(f"'ping' Executed by @{interaction.user} in #{interaction.channel} | {interaction.guild}")
+
+
+
 client.run(TOKEN, root_logger=True)
